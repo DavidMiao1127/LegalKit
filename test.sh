7e7b565029d995e8c3357774affdb18b04e949f1
DavidMiao1127 <miaos22@mails.tsinghua.edu.cn>
2025-09-30 01:22:26 +0800

    fix: Chinese README.md

--- test.sh
+++
@@ -1,4 +0,0 @@
-MODELS="/data/glm-train-prod/SFT/Qwen3-4B-Base/iter_0010000/HF"
-
-python legalkit/main.py --config example/config_jecqa.yaml --models $MODELS
-# python legalkit/main.py --config example/config_lexeval.yaml --models $MODELS
--- test_2.sh
+++
@@ -1,4 +0,0 @@
-MODELS="/data/ms/ms-swift/megatron_output/Qwen3-4B-SFT/v86-20250808-011350/HF"
-
-python legalkit/main.py --config example/config_jecqa.yaml --models $MODELS
-# python legalkit/main.py --config example/config_lexeval.yaml --models $MODELS
